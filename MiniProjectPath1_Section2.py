34a76b6d23be4a3a43ae064e4ee9147e71283442
juliejoffe <49820332+juliejoffe@users.noreply.github.com>
2022-07-29 08:58:09 -0400

    Update MiniProjectPath1_Section2.py

--- MiniProjectPath1_Section2.py
+++ MiniProjectPath1_Section2.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 
 dfTempVsTraffic = pd.read_csv('NYC_Bicycle_Counts_2016_Corrected.csv', usecols = ['High Temp', 'Low Temp', 'Total'])
 tempVals = dfTempVsTraffic[['High Temp', 'Low Temp']]
-dfTempVsTraffic ['Average Temp'] = tempVals.mean(numeric_only = True, axis = 1)
+#print (dfTempVsTraffic.info())
+dfTempVsTraffic ['Average Temp'] = tempVals.mean(axis = 1) #uses high and low temp columns to calculate average temp column
 
-print (dfTempVsTraffic)
+dfTempVsTraffic['Total'] = dfTempVsTraffic['Total'].str.replace(',','') #removes commas from total column
+dfTempVsTraffic['Total'] = dfTempVsTraffic ['Total'].astype(float, errors = 'raise') #converts total column from object type to float
+#print (dfTempVsTraffic.info())
 
+#print (dfTempVsTraffic)
+
+#creating and plotting the graph
 x = dfTempVsTraffic['Average Temp']
-print("x: ", x)
 y = dfTempVsTraffic['Total']
-print("y: ", y)
 plt.scatter(x,y)
-plt.xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
-#plt.yticks([0, 5000, 10000, 15000, 20000, 25000, 30000])
-plt.xlabel('Average Temperature')
+plt.xticks([30, 40, 50, 60, 70, 80, 90, 100])
+plt.yticks([0, 5000, 10000, 15000, 20000, 25000, 30000])
+plt.grid()
+plt.xlabel('Average Temperature ($^\circ$F)')
 plt.ylabel('Total Bike Traffic')
-plt.title('Average Temperature vs Total Bike Traffic')
+plt.title('Average Temperature Vs. Total Bike Traffic')
 plt.show()
